efa55fb4b6942dec9a41e01fc44fb32b06851d10
Kim0Jae <youngjae1047@naver.com>
2017-10-12 03:14:25 +0900

    Initial commit

---
+++ src/1001.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main(args : Array<String>) {
+    //두 수를 입력받고 뺄셈을 한 결과를 출력하는 문제
+    val scan = Scanner(System.`in`)
+
+    val a = scan.nextInt()
+    val b = scan.nextInt()
+
+    System.out.println(a-b)
+}
---
+++ src/2577.kt
@@ -0,0 +1,4 @@
+fun main(args : Array<String>) {
+    //Hello World! 를 화면에 출력하는 문제
+    System.out.println("Hello World!")
+}
