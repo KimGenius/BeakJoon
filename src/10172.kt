050541140dfcfcf2b3639bd7d20d4e890d26896a
Kim0Jae <youngjae1047@naver.com>
2017-10-12 16:47:59 +0900

    10172번 문제 추가

---
+++ src/10172.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    System.out.print("|\\_/|\n" +
+            "|q p|   /}\n" +
+            "( 0 )\"\"\"\\\n" +
+            "|\"^\"`    |\n" +
+            "||_/=\\\\__|")
+}
